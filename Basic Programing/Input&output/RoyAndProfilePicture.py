8f850fb2e6152cec9b33e611dab7a720b8d0bb93
Rathinavelkumar <Rathinavelkumar.Murugan@gmail.com>
2021-09-25 11:56:03 +0530

    updated script

--- Basic Programing/Input&output/RoyAndProfilePicture.py
+++ Basic Programing/Input&output/RoyAndProfilePicture.py
@@ -3,9 +3,9 @@ photos_count = int(input())
 
 for each_photo in range(0, photos_count):
     width, height = map(int, input().split(" "))
-    if width==length and height==length:
-        print("ACCEPTED")
-    elif width<length or height<length:
+    if width<length or height<length:
         print("UPLOAD ANOTHER")
+    elif width==height:
+        print("ACCEPTED")
     else:
         print("CROP IT")
