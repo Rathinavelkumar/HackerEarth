d9b9e395106e69ce8b44a5a10f80ff5aaf2b5888
Rathinavelkumar <Rathinavelkumar.Murugan@gmail.com>
2021-10-21 09:52:47 +0530

    updated code

--- Algorithms/LinearSearch/HexadecimalNumbers.py
+++ Algorithms/LinearSearch/HexadecimalNumbers.py
@@ -2,10 +2,15 @@ import math
 
 def find_hex_decimal(num1, num2):
     count=0
-    for i in range (num1, num2):
+    #Iterating each number between the given range
+    for i in range(num1, num2):
         total=0
+        #Get the hex value of given digit and remove the "0x"
         for each in hex(i).lstrip("0x"):
+            #int(each,16) helps to covert a hex number to integer
             total=total+int(each,16)
+        #Finally check the greatest common divisor
+        #If, its greater than 1, increment the count
         if(math.gcd(i,total)>1):
             count=count+1
     return count
@@ -13,4 +18,4 @@ def find_hex_decimal(num1, num2):
 tc = int(input())
 for _ in range(tc):
     L, R = map(int, input().split())
-    print(find_hex_decimal(L, R+1))
+    print(find_hex_decimal(L,R+1))
